ac4bc9f93489869e3dcb2a51b179eada3caeecb1
naherrma <naherrma@ncsu.edu>
2018-06-14 19:59:35 -0400

    Project 2 original code

---
+++ Project 2/Objects/spi_io.d
@@ -0,0 +1,10 @@
+.\objects\spi_io.o: Source\spi_io.c
+.\objects\spi_io.o: Source\spi_io.h
+.\objects\spi_io.o: Source\integer.h
+.\objects\spi_io.o: D:\MDK\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\spi_io.o: D:\MDK\ARM\PACK\Keil\Kinetis_KLxx_DFP\1.14.0\Device\Include\MKL25Z4.h
+.\objects\spi_io.o: D:\MDK\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\core_cm0plus.h
+.\objects\spi_io.o: D:\MDK\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\cmsis_compiler.h
+.\objects\spi_io.o: D:\MDK\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\cmsis_armcc.h
+.\objects\spi_io.o: .\RTE\Device\MKL25Z128xxx4\system_MKL25Z4.h
+.\objects\spi_io.o: Source\debug.h
